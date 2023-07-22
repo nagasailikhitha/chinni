feb335f3ccf051a1ad3e33beebfd60947604017a
nagasailikhitha <n.s.likhithach@gmail.com>
2023-07-22 14:24:24 +0530

    Navbar adding

--- frontendd/frontend/src/components/Navbar/index.js
+++ frontendd/frontend/src/components/Navbar/index.js
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
 import { Link } from 'react-router-dom';
 
 const pages = ['Home', 'Movies', 'Blog'];
@@ -40,7 +40,7 @@ function Navbar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          
           <Typography
             variant="h6"
             noWrap
@@ -56,7 +56,8 @@ function Navbar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            <LocalMoviesIcon/>
+            BookMyShow
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -95,7 +96,7 @@ function Navbar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+        
           <Typography
             variant="h5"
             noWrap
